230ad5db284790c6e686b6122492d88c853d9b8b
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-12-25 21:03:36 -0500

    Work on Styling
    Finished styles for mobile versions of user dashboard and topic-stats.
    
    Still need mobile version of CSS for the other components, desktop CSS for all components, and typescript files.

--- angular-frontend/src/app/user/topic-stats/topic-stats.component.ts
+++ angular-frontend/src/app/user/topic-stats/topic-stats.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-topic-stats',
   templateUrl: './topic-stats.component.html',
-  styleUrl: './topic-stats.component.css'
+  styleUrls: ['./topic-stats.component.css']
 })
 export class TopicStatsComponent {
+
 
 }
